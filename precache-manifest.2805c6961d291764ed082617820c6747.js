2f602e0ec06c33a65821c10fd9d4d5df2f7ea33c
valyakuttan <valyakuttan@noreply.com>
2019-01-18 17:54:49 +0530

    react app

---
+++ precache-manifest.2805c6961d291764ed082617820c6747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4153e0d3756bc5bdecd",
+    "url": "/static/css/main.4422deea.chunk.css"
+  },
+  {
+    "revision": "f4153e0d3756bc5bdecd",
+    "url": "/static/js/main.f4153e0d.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c574f2b8bca2288e0a47b94e6a5ded62",
+    "url": "/index.html"
+  }
+];
